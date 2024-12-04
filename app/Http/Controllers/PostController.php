38b7545f9b49a582efc86e1652c047af00abbd75
Ulia Rizki Maulana <56014907+uliarizki@users.noreply.github.com>
2024-12-04 15:37:21 +0700

    v0.2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,21 +2,23 @@
 
 namespace App\Http\Controllers;
 
-
-use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
-}
+    /**
+     * index
+     * 
+     * @rturn void
+     */
 
-public function index(){
-    //get posts
+    public function index()
+    {
+        //get posts
+        $posts = Post::latest()->paginate(5);
 
-    $posts = Post::latest()->paginate(5);
-
-    //render view with post
-    return view('posts.index', compact('posts'));
-
+        //render view with posts
+        return view('posts.index', compact('posts'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Builder\Function_;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/halo', Function (){
+//     echo "Halo";
+// });
+
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
